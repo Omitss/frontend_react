579befa426c2e2f4601205d5f60356e936edb70f
omits <omits1118@gmail.com>
2026-05-13 19:42:02 +0900

    thrid commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,15 @@ const scores = [ // table구조 == JSON구조
   {이름 : "Sue", 국어: 90, 영어: 90, 수학:80, 과학 : 90}
 ]
 
-const sources = [0,1,2,3]
-tmp = []
-for(let item of sources){
-  tmp.push(item*2)
-}
+// // 구 시대적 맵핑방식
+// const sources = [0,1,2,3]
+// tmp = []
+// for(let item of sources){
+//   tmp.push(item*2)
+// }
 
-sources.map(item=>(item*2))
+// //지금의 맵핑 방식(JS문법)
+// sources.map(item=>(item*2))
 
 function App() {
 
@@ -27,20 +29,28 @@ function App() {
     <table>
      
       <tr>
-        <th>이름</th>
-        <th>국어</th>
-        <th>영어</th>
-        <th>수학</th>
-        <th>과학</th>
-      </tr>
-      {/* map()함수를 이용해서 코드를 효과적으로 줄임 */}
-      {[0,1,2,3].map(idx=> (
+        {// scores[0]이라는 object를 Object.keys(scores[0])로 list로 만듬
+        // 왜 list로 만들어? for문을 도는 것은 list임. Object는 for문을 돌 수 없음 
+        // 그래서 Object.keys로 key값만, Object.values로 values값만 추출해서 map이라는 함수로 자동적으로 list로 만들어 주는 거임
+
+        // 2문장으로 코드가 많이 줄어듬
+        
+          Object.keys(scores[0]).map( key=>(
+              <th>{key}</th>
+          ))
+        }      
+      </tr>      
+      {// 매개변수 괄호 (key) vs key
+        //매개변수가 2개 이상일 때: map((key, index) => (...))
+        //매개변수가 1개: map(key => (...))
+      }
+      {scores.map(item=> (
       <tr>            
-        <td>{scores[idx].이름}</td>
-        <td>{scores[idx].국어}</td>
-        <td>{scores[idx].영어}</td>
-        <td>{scores[idx].수학}</td>
-        <td>{scores[idx].과학}</td>  
+        {
+        Object.values(item).map(
+          (value) => (<td>{value}</td>)
+        )
+        }
       </tr>
       ))
     }
@@ -49,4 +59,3 @@ function App() {
 }
 
 export default App
-
